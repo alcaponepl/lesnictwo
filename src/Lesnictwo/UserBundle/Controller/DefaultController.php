aa93960237d99f8b25e33c7606faebbc817eb1d9
Piotr <Piotr@PLUS>
2015-01-24 12:24:46 +0100

    Dodanie LesnictwoUserBundle oraz usunięcie zbędnych bundli

---
+++ src/Lesnictwo/UserBundle/Controller/DefaultController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Lesnictwo\UserBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller
+{
+    public function indexAction($name)
+    {
+        return $this->render('LesnictwoUserBundle:Default:index.html.twig', array('name' => $name));
+    }
+}
---
+++ src/Lesnictwo/UserBundle/LesnictwoUserBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Lesnictwo\UserBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class LesnictwoUserBundle extends Bundle
+{
+}
